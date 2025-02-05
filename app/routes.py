a9251ad299009eb579ea243b2f245454bd0b572b
Andrew Lim <133227688+AndyLimHub@users.noreply.github.com>
2025-02-05 14:22:09 -0300

    Added followers and an explore page

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,8 @@ from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 from app.forms import EmptyForm
+from app.forms import PostForm
+from app.models import Post
 
 
 @app.before_request
@@ -16,10 +18,17 @@ def before_request():
         db.session.commit()
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
     posts = [
         {
             'author': {'username': 'John'},
@@ -30,7 +39,8 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', posts=posts)
+    return render_template("index.html", title='Home Page', form=form,
+                           posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -141,3 +151,11 @@ def follow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
+    
+@app.route('/explore')
+@login_required
+def explore():
+    query = sa.select(Post).order_by(Post.timestamp.desc())
+    form = PostForm()
+    posts = db.session.scalars(query).all()
+    return render_template('index.html', title='Explore', posts=posts)
